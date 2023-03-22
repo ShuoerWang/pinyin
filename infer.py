1adcd2d9757999f0fea68c18e72b9390e206c4f2
Herous Vag <shuoer.wang@mail.utoronto.ca>
2023-03-22 13:02:39 -0400

    Update infer.py

--- infer.py
+++ infer.py
@@ -9,8 +9,7 @@ if __name__=='__main__':
     special_tokens = ["<extra_id_{}>".format(i) for i in range(100)]
     tokenizer = T5Tokenizer.from_pretrained("IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese", additional_special_tokens=special_tokens,)
     input = tokenizer.encode(text, return_tensors="pt")
-    config = T5Config.from_pretrained("IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese")
-    model = T5ForConditionalGeneration.from_pretrained("IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese", config=config)
+    model = T5ForConditionalGeneration.from_pretrained(path)
     model.resize_token_embeddings(len(tokenizer))
     output=model.generate(input)
     print(tokenizer.decode(output[0]))
