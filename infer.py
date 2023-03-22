70a19ae44b2f6bcde8e23be5e483c55f56d54d59
Herous Vag <shuoer.wang@mail.utoronto.ca>
2023-03-22 01:24:04 -0400

    Update infer.py

--- infer.py
+++ infer.py
@@ -2,13 +2,13 @@ from transformers import BertTokenizer, T5Config, T5Tokenizer, T5ForConditionalG
 import sys
 
 if __name__=='__main__':
-    text = sys.argv[2]
     path = sys.argv[1]
+    text = sys.argv[2]
     # tokenizer = BertTokenizer.from_pretrained("uer/t5-small-chinese-cluecorpussmall")
     # model = T5ForConditionalGeneration.from_pretrained(path)
-    # input = tokenizer.encode(text, return_tensors="pt")
     special_tokens = ["<extra_id_{}>".format(i) for i in range(100)]
     tokenizer = T5Tokenizer.from_pretrained("IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese", additional_special_tokens=special_tokens,)
+    input = tokenizer.encode(text, return_tensors="pt")
     config = T5Config.from_pretrained("IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese")
     model = T5ForConditionalGeneration.from_pretrained("IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese", config=config)
     model.resize_token_embeddings(len(tokenizer))
